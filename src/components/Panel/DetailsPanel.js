9d07ce0f7e078f42266c4ff0a685f1d56f59e314
vicky-straive <vignesh.muralimohan@straive.com>
2024-05-23 16:43:05 +0530

    row update completed

--- src/components/Panel/DetailsPanel.js
+++ src/components/Panel/DetailsPanel.js
@@ -14,6 +14,30 @@ export default function TemplateDemo({}) {
 
  ; // Empty dependency array to run the effect only once on mount
 
+ const submit = async () => {
+  // try {
+  //   // Make an API call to save the updated data
+  //   const apiUrl = `${SER_BASE_CONNECTION}/api/saveIDMLData`;
+  //   const response = await axios.post(
+  //     apiUrl, JSON.stringify(payload),
+  //     {
+  //       headers: {
+  //         "Content-Type": "application/x-www-form-urlencoded",
+  //         Authorization: `Bearer ${token}`,
+  //       },
+  //     },
+  //   );
+
+  //   if (response.status === 200) {
+  //     console.log("Row data saved successfully");
+  //   } else {
+  //     console.error("Failed to save row data");
+  //   }
+  // } catch (error) {
+  //   console.error("Error saving row data:", error);
+  // }
+ }
+
   const headerTemplate = (options) => {
     const className = `${options.className} justify-content-space-between`;
 
@@ -25,7 +49,7 @@ export default function TemplateDemo({}) {
           <p className="m-0">Chapter ID: {rowData?.CHAPTER_ID}</p>
           <p className="m-0">Chapter Name: {rowData?.CHAPTER_NAME}</p>
         </div>
-        <Button label="Save" />
+        <Button label="Submit" onClick={submit} />
       </div>
     );
   };
--- src/components/Table/PrimeTable.js
+++ src/components/Table/PrimeTable.js
@@ -1,4 +1,3 @@
-
 import React, { useEffect, useState } from "react";
 import { DataTable } from "primereact/datatable";
 import { Column } from "primereact/column";
@@ -14,11 +13,11 @@ import { useRowEditingState } from "primereact/hooks";
 import { useLocation } from "react-router-dom";
 import API_CONSTANTS from "../../Service/API_Configs";
 import axios from "axios";
-import moment from 'moment';
+import moment from "moment";
+import { useSessionStorage } from "primereact/hooks";
 
 export default function RowEditingDemo() {
   const [products, setProducts] = useState(null);
-  // const [statuses] = useState(["MODIFIED", "NOT MODIFIED"]);
   const [modDate, setModData] = useState(false);
   const [selectedRow, setSelectedRow] = useState(null);
   const [rowEditorState, setRowEditorState] = useState(null);
@@ -26,50 +25,50 @@ export default function RowEditingDemo() {
   const rowData = location.state;
   const { SER_BASE_CONNECTION, BASE_TOKEN_CONNECTION } = API_CONSTANTS;
   const [IDMLData, setIDMLData] = useState(null);
-  // console.log("data in Prime", rowData);
-  console.log("IDML data", IDMLData);
-  // console.log("IDML data node", IDMLData.data.SOURCE_CONTENT);
+  const [token, setToken] = useSessionStorage("", "token");
+  const [editedRowData, setEditedRowData] = useState();
 
-  useEffect(() => {
-    const getRowIDMLData = async (rowData) => {
-      try {
-        const apiUrl = `${SER_BASE_CONNECTION}/api/getIDMLData`;
-        const response = await axios.post(
-          apiUrl,
-          { id: rowData.ID },
-          {
-            headers: {
-              "Content-Type": "application/x-www-form-urlencoded",
-              // Authorization: `Bearer ${token}`,
-            },
-          }
-        );
+  // console.log("session", token);
 
-        if (response.data ) {
-          const flattenedData = response.data.data.data.map((item) => ({
-            ...item,
-            id: item.ID, // Assuming the table expects an 'id' field
-          }));
-          console.log("res", response);
-          setIDMLData(flattenedData);
-        } else {
-          console.error("Unexpected response data format:", response.data);
-          setIDMLData(null);
+  const getRowIDMLData = async (rowData) => {
+    try {
+      const apiUrl = `${SER_BASE_CONNECTION}/api/getIDMLData`;
+      const response = await axios.post(
+        apiUrl,
+        { id: rowData.ID },
+        {
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+            // Authorization: `Bearer ${token}`,
+          },
         }
-      } catch (error) {
-        console.error("Error fetching data:", error);
+      );
+
+      if (response.data) {
+        const flattenedData = response.data.data.data.map((item) => ({
+          ...item,
+          id: item.ID, // Assuming the table expects an 'id' field
+        }));
+        console.log("res", response);
+        setIDMLData(flattenedData);
+      } else {
+        console.error("Unexpected response data format:", response.data);
         setIDMLData(null);
       }
-    };
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      setIDMLData(null);
+    }
+  };
 
-    // Call the function with rowData on component mount
+  // Call the function with rowData on component mount
+  
+  useEffect(() => {
+    ProductService.getProductsMini().then((data) => setProducts(data));
     if (rowData) {
       getRowIDMLData(rowData);
     }
-  }, []);
-
-  useEffect(() => {
-    ProductService.getProductsMini().then((data) => setProducts(data));
+  
   }, []);
 
   const allowEdit = (rowData) => {
@@ -91,51 +90,67 @@ export default function RowEditingDemo() {
         return null;
     }
   };
-  
-  const onRowEditComplete = (e) => {
+
+  const onRowEditComplete = async (e) => {
     if (IDMLData) {
       let _IDMLData = [...IDMLData];
       let { newData, index } = e;
-  
+
+      setEditedRowData(newData);
+      console.log("Edited Row Data:", editedRowData?.TRANSLATED_CONTENT);
+      console.log("Edited Row Data:", editedRowData?.ID);
+
       // Compare the original content with the modified content
-      const isContentModified = _IDMLData[index].TRANSLATED_CONTENT !== newData.TRANSLATED_CONTENT;
-  
+      const isContentModified =
+        _IDMLData[index]?.TRANSLATED_CONTENT !== newData?.TRANSLATED_CONTENT;
+
       // Check if CREATED_DATE and MODIFIED_DATE are the same
       const createdDate = moment(_IDMLData[index].CREATED_DATE);
       const modifiedDate = moment(_IDMLData[index].MODIFIED_DATE);
       const isDateModified = !createdDate.isSame(modifiedDate);
-      setModData(isDateModified)
-  
+      setModData(isDateModified);
+
       // Update the inventoryStatus based on content and date modification
       if (isContentModified || isDateModified) {
-        console.log("moded");
         newData.inventoryStatus = "MODIFIED";
       } else {
         newData.inventoryStatus = "NOT MODIFIED";
-        console.log("notModed");
-
       }
-  
+
       // Update the MODIFIED_DATE with the current timestamp if modified
       if (newData.inventoryStatus === "MODIFIED") {
-        newData.MODIFIED_DATE = moment().format('YYYY-MM-DD HH:mm:ss');
+        newData.MODIFIED_DATE = moment().format("YYYY-MM-DD HH:mm:ss");
       }
-  
+
       _IDMLData[index] = newData;
       setIDMLData(_IDMLData);
-  
-      // try {
-      //   // Make an API call to save the updated data
-      //   const response = await axios.put(`/api/products/${newData.id}`, newData);
-  
-      //   if (response.status === 200) {
-      //     console.log('Row data saved successfully');
-      //   } else {
-      //     console.error('Failed to save row data');
-      //   }
-      // } catch (error) {
-      //   console.error('Error saving row data:', error);
-      // }
+
+      const payload = [
+        {
+          ID: newData?.ID,
+          TRANSLATED_CONTENT: newData?.TRANSLATED_CONTENT,
+        },
+      ];
+
+      try {
+        // Make an API call to save the updated data
+        const apiUrl = `${SER_BASE_CONNECTION}/api/saveIDMLData`;
+        const response = await axios.post(apiUrl, JSON.stringify(payload), {
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+            Authorization: `Bearer ${token}`,
+          },
+        });
+
+        if (response.status === 200) {
+          console.log("Row data saved successfully");
+          getRowIDMLData(rowData);
+        } else {
+          console.error("Failed to save row data");
+        }
+      } catch (error) {
+        console.error("Error saving row data:", error);
+      }
     }
   };
 
@@ -150,13 +165,11 @@ export default function RowEditingDemo() {
     );
   };
 
-  console.log("da", modDate);
-
   const checker = (rowData) => {
     const isRowEditing = rowData === rowEditorState;
     return (
       <div>
-        {!isRowEditing && rowData.inventoryStatus === "MODIFIED" && modDate && (
+        {!isRowEditing && rowData.inventoryStatus === "MODIFIED" && (
           <i
             className="pi pi-check"
             style={{ color: "#0ac50a", position: "relative", right: "80px" }}
@@ -167,8 +180,6 @@ export default function RowEditingDemo() {
     );
   };
 
-
-
   const pdfViewer = (rowData) => {
     return (
       <div>
@@ -176,8 +187,6 @@ export default function RowEditingDemo() {
       </div>
     );
   };
-
-  
 
   return (
     <div className="card p-fluid db-table">
@@ -200,39 +209,38 @@ export default function RowEditingDemo() {
       >
         {/* <Column field="id" header="Page No" style={{ width: "0%" }}></Column> */}
 
-            <Column
-              field="SOURCE_CONTENT"
-              header="Source Content"
-              style={{ width: "20%" }}
-            ></Column>
-            <Column
-              field="TRANSLATED_CONTENT"
-              header="Machine Translated Content"
-              editor={(options) => textEditor(options)}
-              style={{ width: "20%" }}
-            ></Column>
-            <Column
-              header="Modify"
-              headerStyle={{ width: "0%", minWidth: "8rem" }}
-              rowEditor={allowEdit}
-              bodyStyle={{ textAlign: "" }}
-            ></Column>
-            <Column
-              body={checker}
-              headerStyle={{ width: "0%", minWidth: "rem" }}
-              bodyStyle={{ textAlign: "" }}
-            ></Column>
-            <Column
-              header="PDF View"
-              body={pdfViewer}
-              headerStyle={{
-                width: "20%",
-                minWidth: "rem",
-                textAlign: "center",
-              }}
-              bodyStyle={{ textAlign: "center" }}
-            ></Column>
-         
+        <Column
+          field="SOURCE_CONTENT"
+          header="Source Content"
+          style={{ width: "20%" }}
+        ></Column>
+        <Column
+          field="TRANSLATED_CONTENT"
+          header="Machine Translated Content"
+          editor={(options) => textEditor(options)}
+          style={{ width: "20%" }}
+        ></Column>
+        <Column
+          header="Modify"
+          headerStyle={{ width: "0%", minWidth: "8rem" }}
+          rowEditor={allowEdit}
+          bodyStyle={{ textAlign: "" }}
+        ></Column>
+        <Column
+          body={checker}
+          headerStyle={{ width: "0%", minWidth: "rem" }}
+          bodyStyle={{ textAlign: "" }}
+        ></Column>
+        <Column
+          header="PDF View"
+          body={pdfViewer}
+          headerStyle={{
+            width: "20%",
+            minWidth: "rem",
+            textAlign: "center",
+          }}
+          bodyStyle={{ textAlign: "center" }}
+        ></Column>
       </DataTable>
     </div>
   );
